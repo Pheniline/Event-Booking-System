d91142faebd56442cc75396f54fb37edcbfd1d86
Pheniline <phennyjer@gmail.com>
2026-01-22 23:14:55 +0300

    Event Booking System

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,11 +1,20 @@
 import 'react'from React;
 
 const Navbar = () => {
+    //Navbar links array: storing them in an array so that we can loop later.
     const navLinks = ["Home", "About", "Services", "Contact"];
+    //Creates a state to track if the user has scrolled down the page.
     const [scrolled, setScrolled]= usestate(false);
-
+    //Runs after the component loads(mounts).It  is used to listen for page scroll events.
     useEffect(() => {
         const handleScroll = () => {
+            /*gives the scroll distance in pixels.If user has scrolled more than this value
+            the scrolled becomes true*/
+            if(window.scrollY > 50){
+                setScrolled(true);
+            }else{
+                setScrolled(false);
+            };
             
         }
     })
