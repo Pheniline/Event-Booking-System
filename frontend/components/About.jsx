1826ba5c4c26bcf4ac625222ba61749378ad9ab5
Pheniline <phennyjer@gmail.com>
2026-01-29 23:28:04 +0300

    Event Booking system

--- frontend/components/About.jsx
+++ frontend/components/About.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React from "react"; /*imports the react library*/
 
+/*Defines the funcional component*/
 const About = () => {
+  /*Tells react what to render on the screen*/
   return (
+    /*The about section of the webpage*/
     <section id="about" className="about">
+      /*Heading for the about section*/
       <h2>About</h2>
     </section>
   );
