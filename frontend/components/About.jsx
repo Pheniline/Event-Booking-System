5f4eb08c7b75d8f7fced96922c85fa39c4aad637
Pheniline <phennyjer@gmail.com>
2026-01-29 23:48:04 +0300

    Event Booking System

--- frontend/components/About.jsx
+++ frontend/components/About.jsx
@@ -16,8 +16,9 @@ const About = () => {
         in East Africa, attracting thousands of attendees each year.
       </p>
       <p>
-        This years Solfest will feature a diverse line up of artist {"
+        This years Solfest will feature a diverse line up of artist 
         <span className="artists">Sauti sol,Nyashiski,Toxic Lyrikali,Nikita Keiring,Lil'Maina and many more"</span>}
+        {""}
         Come and experience an unforgettable celebration of music,
         culture,and community at SolFest Africa!
         
