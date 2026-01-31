fed1762e1c70c3a8ea3a5b1bd5c68661b4547058
Pheniline <phennyjer@gmail.com>
2026-01-31 22:21:23 +0300

    Event Booking System

--- frontend/components/Contact.jsx
+++ frontend/components/Contact.jsx
@@ -2,6 +2,11 @@ import "React" from "react";
 
 const Contact = () => {
     return (
-        
+        <section id="contact" className="contact">
+            <h2>Contact us</h2>
+            <div className="contact-methods">
+                
+            </div>
+        </section>
     )
 }
